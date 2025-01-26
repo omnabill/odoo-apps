8f4ae4434d024d2d0dc1a67b33c36068dfc663d9
omar <Omar.Nabil@pan-marine.net>
2025-01-26 09:27:42 +0200

    commit invalidate

--- invalidate_user_session/__manifest__.py
+++ invalidate_user_session/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': """Action to force user to logout during running session, only for administrator of the system.""",
     'description': """Action to force user to logout during running session, only for administrator of the system. """,
     'category': 'Extra Tools',
-    'author': 'OMAR',
+    'author': 'OMAR-NABIL',
     'license': 'AGPL-3',
     "images": ["static/description/banner.png"],
     'depends': ['base'],
